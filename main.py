b6f250353a14e69dcc792a0562befc10a3590b64
phuongdoan13 <mphuongdoant@gmail.com>
2023-09-23 15:04:58 +1000

    Update: logic to wait for the loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import tasks
 import asyncio
 import datetime as dt
 from dotenv import load_dotenv
+from helper import SecondToDesiredHour
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -37,19 +38,19 @@ async def on_message(message):
   response = 'I am a bot that can do many things. Many new features are coming!'
   await message.channel.send(response)
 
-target_hour = 1
-@tasks.loop(seconds=15)
+target_hour = 18
+@tasks.loop(hours=24)
 async def standup_task():
 	now = dt.datetime.now()
 	if now.hour == target_hour:
 		TEST_CHANNEL_ID = int(os.getenv("DISCORD_TEST_CHANNEL_ID"))
 		message_channel = client.get_channel(TEST_CHANNEL_ID)
 		await message_channel.send(
-			"""
-			1. What have you done ?
-			2. What are you planning to work on today ?
-			3. Any blockers ?
-			"""
+"""
+1. What have you done ?
+2. What are you planning to work on today ?
+3. Any blockers ?
+"""
 		)
 	else:
 		hours_until_target = (target_hour - now.hour) % 24
@@ -60,16 +61,9 @@ async def standup_task():
 @standup_task.before_loop
 async def before_reminder():
 	now = dt.datetime.now()
-
-	if now.hour >= target_hour:
-		hours_until_target = 24 - now.hour + target_hour
-	else:
-		hours_until_target = target_hour - now.hour
-	
-	minutes_until_target = 60 - now.minute if now.hour != target_hour else 0
-	seconds_until_target = 60 - now.second
-	total_wait = (hours_until_target * 3600) + (minutes_until_target * 60) - seconds_until_target
-
+	if(now.hour == target_hour):
+		return
+	total_wait = SecondToDesiredHour.get_second_until_desired_hour(target_hour) + 1 # add 1 second margin of error to make sure the time is passed
 	print(f"Waiting for {total_wait} seconds before starting the reminder loop.")
 	await asyncio.sleep(total_wait)
 
